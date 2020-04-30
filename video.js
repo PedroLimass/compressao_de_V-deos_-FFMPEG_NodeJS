6967f627b277916ce2274db3377ac7e2b9e977d5
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-04-30 16:04:04 -0300

    Programa finalizado utilizando o node. ffmpg foi alocado em uma pasta aux.

--- video.js
+++ video.js
@@ -1,6 +1,28 @@
 const { spawn } = require('child_process');
 
-function reSize (video, qualidade){
+
+//node[0] videos[1] ./animes[2]
+const parent = process.argv[2]
+
+let videos = []
+
+
+if(process.argv[2]){//Localizando todos os videos dentro do diretorio
+
+	const start = parseInt(process.argv[3])
+	const end = parseInt(process.argv[4])
+
+		for (let i = start; i <= end; i++) {
+			videos.push(i);
+		}
+		videos.reverse();//Alimentando o vetor com o endereço dos videos
+		processo();// Função para passar os paramentros do videos/endereços e redimenciona
+}else{
+	console.log('É NECESSARIO CRIAR UM DIRETORIO DE NÍVEL SUPERIOR!');
+}
+
+
+function reSize (video, quality){
 	const p = new Promise((resolve,reject)=>{
 		const ffmpeg = spawn('./aux/ffmpeg/bin/ffmpeg',[
 			 '-i',
@@ -23,7 +45,7 @@ function reSize (video, qualidade){
             '0',
             '-b:a',
             '128k',
-            `${parent}/resultado/${video}-${quality}.mp4`
+            `${parent}/resultados/${video}-${quality}.mp4`
 		]);
 		ffmpeg.stderr.on('data',(data)=>{
 			console.log(data);
@@ -35,3 +57,22 @@ function reSize (video, qualidade){
 
 	return p;
 }
+
+
+async function processo () {
+	let video = videos.pop()
+	if (video) {
+		try {
+			await reSize(video, 720)
+			await reSize(video, 480)
+			await reSize(video, 360)
+
+			console.log(`Videos renderizados - ${video}`);
+
+			processo()//recursividade
+		}catch(e){
+			console.log(e)
+		}
+	}
+}
+
